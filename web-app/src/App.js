93fe8d9db50f8a9ade94b838ce8ab4961c063e3e
John Livingston <jmlivingston@gmail.com>
2020-07-01 11:49:02 -0700

    test

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-console.log('xx')
+console.log('xxx')
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
